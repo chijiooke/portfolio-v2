cc55eb616679cc645542b9057eefd9b59f37a8aa
chijooke <michaelsylva36@gmail.com>
2023-05-01 08:59:57 +0100

    chore(fix): scroll button

--- src/components/ScrollGuide.tsx
+++ src/components/ScrollGuide.tsx
@@ -1,13 +1,29 @@
 import { ArrowRight } from "iconsax-react";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
-export const ScrollGuide: FC<{ isAtBottom: boolean }> = ({ isAtBottom }) => {
+export const ScrollGuide: FC<{
+  isAtBottom: boolean;
+  wrapperRef: React.MutableRefObject<HTMLDivElement | null>;
+}> = ({ isAtBottom, wrapperRef }) => {
+  const [windowSrollHeight, setwindowSrollHeight] = useState(0);
+
+  useEffect(() => {
+    if (!!wrapperRef.current) {
+      wrapperRef.current.scrollTop = windowSrollHeight;
+    }
+  }, [windowSrollHeight]);
+
   return (
     <div
       style={{
-        display: `${(isAtBottom || window.screen.width < 800 )? "none" : "flex"}`,
+        display: `${isAtBottom || window.screen.width < 800 ? "none" : "flex"}`,
       }}
       className="scroll__guide"
+      onClick={() => {
+        setwindowSrollHeight(
+          windowSrollHeight + (wrapperRef.current?.offsetHeight || 0)
+        );
+      }}
     >
       Scroll <ArrowRight />
     </div>
